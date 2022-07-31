1ca05908775fa2328aafdaa6b33843ef06cf5f7b
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-07-31 14:51:09 -0300

    A2.4

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import { AppProps } from 'next/app';
 import GlobalStyle from "@src/theme/GlobalStyle";
+import ThemeProvider from "@src/theme/ThemeProvider";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <GlobalStyle />
-      <Component {...pageProps} />
+      <ThemeProvider>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ThemeProvider>
     </>
   )
 }
