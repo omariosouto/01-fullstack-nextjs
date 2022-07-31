6fc37f32ec1d6adfb3651587772eff27c7a92776
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-07-31 18:45:52 -0300

    A4.2

--- src/screens/HomeScreen/patterns/Feed/Feed.tsx
+++ src/screens/HomeScreen/patterns/Feed/Feed.tsx
@@ -4,6 +4,7 @@ import Text from "@src/components/Text/Text";
 import Icon from "@src/components/Icon/Icon";
 import Image from "@src/components/Image/Image";
 import Link from "@src/components/Link/Link";
+import Button from "@src/components/Button/Button";
 
 interface FeedProps {
   children: React.ReactNode;
@@ -22,15 +23,17 @@ export default function Feed({ children }) {
 Feed.Header = () => {
   return (
     <Box>
-      <Image
-        styleSheet={{
-          width: '128px',
-          height: '128px',
-          borderRadius: '100%',
-        }}
-        src="https://github.com/omariosouto.png"
-        alt="Imagem de perfil do Mario Souto"
-      />
+      <Button.Base href="https://github.com/omariosouto">
+        <Image
+          styleSheet={{
+            width: '128px',
+            height: '128px',
+            borderRadius: '100%',
+          }}
+          src="https://github.com/omariosouto.png"
+          alt="Imagem de perfil do Mario Souto"
+        />
+      </Button.Base>
       <Link href="https://youtube.com/DevSoutinho">
         <Icon name="youtube" />
       </Link>
